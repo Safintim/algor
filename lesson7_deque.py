0f4c07854b7b6224063ebfb201bec97eaabf7f8a
Safintim <timurtlt96@mail.ru>
2018-07-06 18:20:10 +0300

    create deque and func polindrom

---
+++ lesson7_deque.py
@@ -0,0 +1,46 @@
+"""
+ДЕК
+"""
+
+
+class Deque:
+
+    def __init__(self):
+        self.queue = []
+
+    def add_front(self, item):
+        self.queue.append(item)
+
+    def add_tail(self, item):
+        self.queue.insert(0, item)
+
+    def remove_front(self):
+        return self.queue.pop()
+
+    def remove_tail(self):
+        return self.queue.pop(0)
+
+    def size(self):
+        return len(self.queue)
+
+
+def palindrom(string):
+
+    d = Deque()
+
+    for ch in string:
+        d.add_front(ch)
+
+    while d.size() > 1:
+
+        if d.remove_front() != d.remove_tail():
+            print('Не является полиндромом', end=' ')
+            return False
+
+    return True
+
+
+test_list = ['довод', 'заказ', 'радар', 'banan', 'apple', 'lsdkjfskf']
+
+for i in test_list:
+    print(palindrom(i))
