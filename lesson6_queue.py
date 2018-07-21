157fc436f640db3cf6384e39eb792cf4f3fe2fcc
Safintim <timurtlt96@mail.ru>
2018-07-21 22:11:06 +0300

    deleting programm exec

--- lesson6_queue.py
+++ lesson6_queue.py
@@ -19,11 +19,6 @@ class Queue:
         return len(self.items)
 
 
-q = Queue()
-test_list = ['Python', 'C++', 'C#', 'Java', 'Ruby', 'R', 'JavaScript', 'PHP',
-             'Django', 'Rails', 'Laravel', 'PyQT5', 'GIT', 'SQL', 'HTML5/CSS3']
-
-
 def add_el(q, l):
     for i in l:
         q.enqueue(i)
@@ -36,12 +31,6 @@ def circle_queue(q, n):
         q.enqueue(q.dequeue())
 
     return q
-
-
-q = add_el(q, test_list)
-# print(q.items)
-# circle_queue(q, 1)
-# print(q.items)
 
 
 # 6.3
@@ -66,24 +55,3 @@ class Queue2:
             return self.stack1.size()
         else:
             return self.stack2.size()
-
-
-
-q2 = Queue2()
-q2 = add_el(q2, test_list)
-print(q2.stack1.stack, 'stack1')
-print(q2.stack2.stack, 'stack2')
-
-# r = ['HTML5/CSS3', 'SQL', 'GIT', 'PyQT5', 'Laravel', 'Rails', 'Django', 'PHP', 'JavaScript', 'R']
-# q2.dequeue()
-# q2.dequeue()
-# q2.dequeue()
-# q2.dequeue()
-# q2.dequeue()
-# circle_queue(q2, 1)
-# print('ПОсле ')
-# print(q2.stack1.stack, 'stack1')
-# print(q2.stack2.stack, 'stack2')
-
-# circle_queue(q, 1)
-# print(q.items)
