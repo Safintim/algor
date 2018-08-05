bbe6f809cd6de3e98d3bda7c72dd0bd2c5014140
Safintim <timurtlt96@mail.ru>
2018-08-06 00:12:55 +0300

    add sort_shell

--- lesson21_sort_insert.py
+++ lesson21_sort_insert.py
@@ -12,10 +12,20 @@ def sort_insert(n, step):
     return n
 
 
-# def sort_insert(n, step):
-#     for i in range(1, len(n)):
-#
-#         for j in range(0, i, step):
-#             if n[j] > n[i]:
-#                 n[j], n[i] = n[i], n[j]
-#     return n
+def sort_shell(n):
+
+    step = 1
+    for i in range(len(n) // 3):
+        step = 3 * step + 1
+
+    while step > 0:
+
+        for i in range(step, len(n)):
+            x = n[i]
+            j = i - step
+            while (j > -1) & (n[j] > x):
+                n[j+step] = n[j]
+                j -= step
+            n[j+step] = x
+        step //= 3
+    return n
--- utest_lesson21.py
+++ utest_lesson21.py
@@ -16,6 +16,18 @@ class InsertSortTest(unittest.TestCase):
         self.assertListEqual(l.sort_insert(n2, 1), result2)
         self.assertListEqual(l.sort_insert(n22, 3), result3)
 
+    def test_sort_shell(self):
+        n1 = [4, 3, 1, 2]
+        n2 = [7, 6, 5, 4, 3, 2, 1]
+        n3 = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        result1 = [1, 2, 3, 4]
+        result2 = [1, 2, 3, 4, 5, 6, 7]
+        result3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        # print(l.sort_insert(n2, 1))
+        self.assertListEqual(l.sort_shell(n1), result1)
+        self.assertListEqual(l.sort_shell(n2), result2)
+        self.assertListEqual(l.sort_shell(n3), result3)
+
 
 if __name__ == '__main__':
     unittest.main()
