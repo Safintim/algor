dcfe827afa94a8ee461d16621d226f8bfa9d13ee
Safintim <timurtlt96@mail.ru>
2018-08-06 21:46:56 +0300

    add sort_fast

--- utest_lesson22.py
+++ utest_lesson22.py
@@ -7,10 +7,10 @@ class FastSortTest(unittest.TestCase):
     def test_sort_insert(self):
         n1 = [4, 3, 1, 2]
         n2 = [7, 6, 5, 4, 3, 2, 1]
-        n3 = [8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        n3 = [8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8, 8]
         result1 = [2, 3, 1, 4]
         result2 = [1, 2, 3, 4, 5, 6, 7]
-        result3 = [8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        result3 = [8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 8, 8]
         self.assertListEqual(l.sort_fast(n1), result1)
         self.assertListEqual(l.sort_fast(n2), result2)
         self.assertListEqual(l.sort_fast(n3), result3)
