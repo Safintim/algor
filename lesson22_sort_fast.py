798eba50b920d81752ce7743b6d7a6c2b9e620e8
Safintim <timurtlt96@mail.ru>
2018-08-06 21:43:15 +0300

    add sort_fast

--- lesson22_sort_fast.py
+++ lesson22_sort_fast.py
@@ -5,26 +5,21 @@ def sort_fast(m):
     n = sum(m) / float(len(m))
 
     if all(n == elem for elem in m):
-       return m
+        return m
     elif all(n > elem for elem in m):
         return m
     elif all(n < elem for elem in m):
         return m
+
     while i1 != (i2 - 1):
 
-        if m[i1] < n:
+        if m[i1] <= n:
             i1 += 1
             continue
-        elif n < m[i2]:
+        elif n <= m[i2]:
             i2 -= 1
             continue
         elif m[i1] > n > m[i2]:
             m[i1], m[i2] = m[i2], m[i1]
 
     return m
-
-
-# m = [7, 6, 5, 4, 3, 2, 1]
-# m1 = [5, 5, 5, 5, 5, 5]
-# m2 = [4, 3, 1, 2]
-# print(sort_fast(m2))
