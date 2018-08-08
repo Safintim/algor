158cd21972e228c7207f6a45a72534dcbc72f327
Safintim <timurtlt96@mail.ru>
2018-08-08 14:41:31 +0300

    create fast sort

--- lesson22_sort_fast.py
+++ lesson22_sort_fast.py
@@ -1,46 +1,19 @@
 def sort_fast(m, left, right):
+    if left >= right:
+        return
     i1 = left
     i2 = right
     n = m[right]
 
-    while i1 != (i2 - 1):
-
-        if m[i1] <= n:
+    while i1 <= i2:
+        while m[i1] < n:
             i1 += 1
-            continue
-        elif n <= m[i2]:
+        while m[i2] > n:
             i2 -= 1
-            continue
-        elif m[i1] > n > m[i2]:
+
+        if i1 <= i2:
             m[i1], m[i2] = m[i2], m[i1]
-
-    if m[i1] < n:
-        sort_fast(m, left, i2 - 1)
-    if m[i2] > n:
-        sort_fast(m, i2 + 1, right)
-    # return m, i1, i2
-
-
-n3 = [8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8, 8]
-print(sort_fast(n3, 0, len(n3)-1))
-print(n3)
-# def sort_fast(m, left, right):
-#     n = m[right]
-
-#     while left < right:
-#         # алгоритм разбиения
-#         if m[left] <= n:
-#             left += 1
-#             continue
-#         elif n <= m[right]:
-#             right -= 1
-#             continue
-#         elif m[left] > n > m[right]:
-#             m[left], m[right] = m[right], m[left]
-
-#     if m[left] < n:
-#         sort_fast(m, left, right - 1)
-#     if m[right] > n:
-#         sort_fast(m, right + 1, right)
-
-#     return m, left, right
+            i1 += 1
+            i2 -= 1
+    sort_fast(m, left, i2)
+    sort_fast(m, i1, right)
--- utest_lesson22.py
+++ utest_lesson22.py
@@ -1,34 +1,29 @@
 import unittest
 import lesson22_sort_fast as l
+import random
+from lesson21_sort_insert import sort_shell
 
 
 class FastSortTest(unittest.TestCase):
 
     def test_sort_fast(self):
-        n1 = [4, 3, 1, 2]
-        n2 = [7, 6, 5, 4, 3, 2, 1]
-        n3 = [8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8, 8]
-        result1 = [2, 3, 1, 4]
-        result2 = [1, 2, 3, 4, 5, 6, 7]
-        result3 = [8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 8, 8]
-        self.assertListEqual(l.sort_fast(n1)[0], result1)
-        self.assertListEqual(l.sort_fast(n2)[0], result2)
-        self.assertListEqual(l.sort_fast(n3)[0], result3)
+        n1 = [8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8, 8]
+        n2 = [12, 16, 6, 1, 20, 11, 4, 10, 3, 13, 9, 1, 2, 5, 10, 12, 1, 18, 3, 11]
+        result1 = [1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 9, 10, 11, 12, 13, 14, 15]
+        result2 = [1, 1, 1, 2, 3, 3, 4, 5, 6, 9, 10, 10, 11, 11, 12, 12, 13, 16, 18, 20]
+        l.sort_fast(n1, 0, len(n1) - 1)
+        l.sort_fast(n2, 0, len(n2) - 1)
+        self.assertListEqual(n1, result1)
+        self.assertListEqual(n2, result2)
+
+    def test_sort_shell(self):
+        test = [random.randint(0, pow(10, 5)) for i in range(5 * pow(10, 5))]
+        sort_shell(test)
 
     def test_sort_fast2(self):
-        n1 = [4, 3, 1, 2]
-        n2 = [7, 6, 5, 4, 3, 2, 1]
-        n3 = [8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8, 8]
-        result1 = l.sort_fast(n1)
-        result2 = l.sort_fast(n2)
-        result3 = l.sort_fast(n3)
+        test = [random.randint(0, pow(10, 5)) for i in range(5 * pow(10, 5))]
+        l.sort_fast(test, 0, len(test)-1)
 
-        self.assertEqual(result1[1], 1)
-        self.assertEqual(result1[2], 2)
-        self.assertEqual(result2[1], 3)
-        self.assertEqual(result2[2], 4)
-        self.assertEqual(result3[1], 8)
-        self.assertEqual(result3[2], 9)
 
 
 if __name__ == '__main__':
