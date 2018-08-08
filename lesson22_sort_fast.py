9653d4a0c9f685dc82ff60154659f9c1ad5ef706
Safintim <timurtlt96@mail.ru>
2018-08-08 13:15:53 +0300

    fix

--- lesson22_sort_fast.py
+++ lesson22_sort_fast.py
@@ -1,15 +1,7 @@
-
-def sort_fast(m):
-    i1 = 0
-    i2 = len(m) - 1
-    n = sum(m) / float(len(m))
-
-    if all(n == elem for elem in m):
-        return m
-    elif all(n > elem for elem in m):
-        return m
-    elif all(n < elem for elem in m):
-        return m
+def sort_fast(m, left, right):
+    i1 = left
+    i2 = right
+    n = m[right]
 
     while i1 != (i2 - 1):
 
@@ -22,4 +14,33 @@ def sort_fast(m):
         elif m[i1] > n > m[i2]:
             m[i1], m[i2] = m[i2], m[i1]
 
-    return m, i1, i2
+    if m[i1] < n:
+        sort_fast(m, left, i2 - 1)
+    if m[i2] > n:
+        sort_fast(m, i2 + 1, right)
+    # return m, i1, i2
+
+
+n3 = [8, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 8, 8]
+print(sort_fast(n3, 0, len(n3)-1))
+print(n3)
+# def sort_fast(m, left, right):
+#     n = m[right]
+
+#     while left < right:
+#         # алгоритм разбиения
+#         if m[left] <= n:
+#             left += 1
+#             continue
+#         elif n <= m[right]:
+#             right -= 1
+#             continue
+#         elif m[left] > n > m[right]:
+#             m[left], m[right] = m[right], m[left]
+
+#     if m[left] < n:
+#         sort_fast(m, left, right - 1)
+#     if m[right] > n:
+#         sort_fast(m, right + 1, right)
+
+#     return m, left, right
