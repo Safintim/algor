090efd2afaa7ac2ae43ff820db35a706ee2bc12e
Safintim <Sarami96>
2018-08-30 14:24:28 +0300

    create func find_start_end

--- lesson25_AST.py
+++ lesson25_AST.py
@@ -1,50 +1,58 @@
+def find_start_end(flag, result, i):
+    """
+    Находит начало и конец аргументов
+    """
+
+    brackets = 1
+    if flag:
+        j = i + 2
+        while brackets != 0:
+            if result[j] == '(':
+                brackets += 1
+            elif result[j] == ')':
+                brackets -= 1
+            j += 1
+    else:
+        j = i - 2
+        while brackets != 0:
+            if result[j] == '(':
+                brackets -= 1
+            elif result[j] == ')':
+                brackets += 1
+            j -= 1
+
+    return j
+
+
 def add_brackets(operator, s):
     result = s.split()
     i = 0
     while i <= len(result) - 1:
         if result[i] == operator:
             if i + 2 <= len(result) - 1:  # чтобы избежать IndexError
+
                 if result[i - 1] == ')':  # если перед стоит закр скобка, то нужно найти начало аргумента
-                    brackets = 1
-                    j = i - 2
-                    while brackets != 0:
-                        if result[j] == '(':
-                            brackets -= 1
-                        elif result[j] == ')':
-                            brackets += 1
-                        j -= 1
+                    j = find_start_end(False, result, i)
                     result.insert(j + 1, '(')
                     i += 1
                 elif result[i - 2] != '(':  # если нет скобочек, то поставить скобочку
                     result.insert(i - 1, '(')
                     i += 1
+
                 if result[i+1] == '(':  # если после стоит откр скобка, то нужно найти конец аргумента
-                    brackets = 1
-                    j = i + 2
-                    while brackets != 0:
-                        if result[j] == '(':
-                            brackets += 1
-                        elif result[j] == ')':
-                            brackets -= 1
-                        j += 1
+                    j = find_start_end(True, result, i)
                     result.insert(j, ')')
                 elif result[i+2] != ')':  # если нет, то поставить
                     result.insert(i+2, ')')
                     i += 1
+
             else:  # если сюда попали, значит, это конец строки и в конце нет скобки
                 result.insert(i + 3, ')')
-                # код повторяется. это плохо
+
                 if result[i - 1] != ')':
                     result.insert(i - 1, '(')
                 if result[i - 1] == ')':
-                    brackets = 1
-                    j = i - 2
-                    while brackets != 0:
-                        if result[j] == '(':
-                            brackets -= 1
-                        elif result[j] == ')':
-                            brackets += 1
-                        j -= 1
+                    j = find_start_end(False, result, i)
                     result.insert(j + 1, '(')
                     i += 1
 
