a8940a2c2e7e48b70984bfd9d8564b47dc4c4732
Safintim <Sarami96>
2018-09-01 09:24:21 +0300

    create AST, ParseExpression

--- lesson25_AST.py
+++ lesson25_AST.py
@@ -1,111 +1,132 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+from lesson14_tree import SimpleTree, TreeNode
 
 
-def find_start_end(flag, result, i):
-    """
-    Находит начало и конец аргументов
-    """
-    brackets = 1
-    if flag:
-        j = i + 2
-        while brackets != 0:
-            if result[j] == '(':
-                brackets += 1
-            elif result[j] == ')':
-                brackets -= 1
-            j += 1
-    else:
-        j = i - 2
-        while brackets != 0:
-            if result[j] == '(':
-                brackets -= 1
-            elif result[j] == ')':
-                brackets += 1
-            j -= 1
-
-    return j
-
-
-def add_brackets(operator, s):
-    result = s.split()
-    i = 0
-    while i <= len(result) - 1:
-        if result[i] == operator:
-            if i + 2 <= len(result) - 1:  # чтобы избежать IndexError
-
-                if result[i - 1] == ')':  # если перед стоит закр скобка, то нужно найти начало аргумента
-                    j = find_start_end(False, result, i)
-                    result.insert(j + 1, '(')
-                    i += 1
-                elif result[i - 2] != '(':  # если нет скобочек, то поставить скобочку
-                    result.insert(i - 1, '(')
-                    i += 1
-
-                if result[i+1] == '(':  # если после стоит откр скобка, то нужно найти конец аргумента
-                    j = find_start_end(True, result, i)
-                    result.insert(j, ')')
-                    i += 1
-                elif result[i+2] != ')':  # если нет, то поставить
-                    result.insert(i+2, ')')
-                    i += 1
-
-            else:  # если сюда попали, значит, это конец строки и в конце нет скобки
-                result.insert(i + 2, ')')
-
-                if result[i - 1] != ')':
-                    result.insert(i - 1, '(')
-                if result[i - 1] == ')':
-                    j = find_start_end(False, result, i)
-                    result.insert(j + 1, '(')
-                    i += 1
-
-        i += 1
-    return ' '. join(result)
-
-
-# i1 = add_brackets('/', '7 + 3 / 25 * ( 5 - 2 ) ')
-# print(i1)
-# i2 = add_brackets('*', i1)
-# print(i2)
-# i3 = add_brackets('-', i2)
-# print(i3)
-# i4 = add_brackets('+', i3)
-# print(i4)
-
+# Узел AST
 class ANode:
     def __init__(self, token_type, token_value):
         self.token_type = token_type
         self.token_value = token_value
 
 
-def parse_exp(exp):
-        operators = ['/', '*', '-', '+']
-        brackets = ['(', ')']
-        token_list = []
+# Парсер выражения
+class ParseExpression:
 
-        for op in operators:
-            exp = add_brackets(op, exp)
+    def __init__(self):
+        self.OPERATOR = 300
+        self.NUMBER = 200
+        self.OPEN_BRACKET = 100
+        self.CLOSE_BRACKET = 101
 
-        exp = exp.split()
+    @staticmethod
+    def find_start_end(flag, result, i):
+        """
+        Находит начало и конец аргументов
+        """
+        brackets = 1
+        if flag:
+            j = i + 2
+            while brackets != 0:
+                if result[j] == '(':
+                    brackets += 1
+                elif result[j] == ')':
+                    brackets -= 1
+                j += 1
+        else:
+            j = i - 2
+            while brackets != 0:
+                if result[j] == '(':
+                    brackets -= 1
+                elif result[j] == ')':
+                    brackets += 1
+                j -= 1
 
-        for elem in exp:
-            if elem in operators:
-                token_list.append(ANode('операция', elem))
-            elif elem in brackets:
-                token_list.append(ANode('скобка', elem))
-            elif elem != ' ':
-                token_list.append(ANode('число', elem))
+        return j
 
-        return token_list
+    def add_brackets(self, operator, s):
+        result = s.split()
+        i = 0
+        while i <= len(result) - 1:
+            if result[i] == operator:
+                if i + 2 <= len(result) - 1:  # чтобы избежать IndexError
+
+                    if result[i - 1] == ')':  # если перед стоит закр скобка, то нужно найти начало аргумента
+                        j = self.find_start_end(False, result, i)
+                        result.insert(j + 1, '(')
+                        i += 1
+                    elif result[i - 2] != '(':  # если нет скобочек, то поставить скобочку
+                        result.insert(i - 1, '(')
+                        i += 1
+
+                    if result[i+1] == '(':  # если после стоит откр скобка, то нужно найти конец аргумента
+                        j = self.find_start_end(True, result, i)
+                        result.insert(j, ')')
+                        i += 1
+                    elif result[i+2] != ')':  # если нет, то поставить
+                        result.insert(i+2, ')')
+                        i += 1
+
+                else:  # если сюда попали, значит, это конец строки и в конце нет скобки
+                    result.insert(i + 2, ')')
+
+                    if result[i - 1] != ')':
+                        result.insert(i - 1, '(')
+                    if result[i - 1] == ')':
+                        j = self.find_start_end(False, result, i)
+                        result.insert(j + 1, '(')
+                        i += 1
+
+            i += 1
+        return ' '. join(result)
+
+    def parse_exp(self, exp):
+            operators = ['/', '*', '-', '+']
+            brackets = ['(', ')']
+            token_list = []
+
+            for op in operators:
+                exp = self.add_brackets(op, exp)
+
+            exp = exp.split()
+
+            for elem in exp:
+                if elem in operators:
+                    token_list.append(ANode(self.OPERATOR, elem))
+                elif elem in brackets[0]:
+                    token_list.append(ANode(self.OPEN_BRACKET, elem))
+                elif elem in brackets[1]:
+                    token_list.append(ANode(self.CLOSE_BRACKET, elem))
+                elif elem != ' ':
+                    token_list.append(ANode(self.NUMBER, elem))
+
+            return token_list
 
 
-s = [(i.token_type, i.token_value) for i in parse_exp('3 + 5 - 5 + 3')]
-print(s)
-'''
-результат
-[('скобка', '('), ('скобка', '('), ('число', '3'), ('операция', '+'),
-('скобка', '('), ('число', '5'), ('операция', '-'), ('число', '5'),
-('скобка', ')'), ('скобка', ')'), ('операция', '+'), ('число', '3'), 
-('скобка', ')')]
-'''
+# AST
+class AST:
+    def __init__(self):
+        self.tree = SimpleTree(TreeNode(None, None))
+        self.node = self.tree.root
+
+    def create(self, exp):
+        parser = ParseExpression()
+        token_list = parser.parse_exp(exp)
+        index = 0
+
+        while len(token_list) > 0:
+            if token_list[0].token_type == parser.OPEN_BRACKET:
+                token_list.pop(0)
+                self.tree.add_node(self.node, TreeNode(self.node, None))
+                self.node = self.node.child[0]
+            elif token_list[0].token_type == parser.CLOSE_BRACKET:
+                token_list.pop(0)
+                self.node = self.node.parent
+            elif token_list[0].token_type == parser.NUMBER:
+                self.node.value = token_list.pop(0).token_value
+                self.node = self.node.parent
+            elif token_list[0].token_type == parser.OPERATOR:
+                self.node.value = token_list.pop(0).token_value
+                self.tree.add_node(self.node, TreeNode(self.node, None))
+                self.node = self.node.child[1]
+        return self.tree
--- utest_lesson25_AST.py
+++ utest_lesson25_AST.py
@@ -12,26 +12,38 @@ class ASTTest(unittest.TestCase):
         result3 = '( 7 + ( ( 3 * 5 ) - 2 ) )'
 
         test_operators = ['/', '*', '-', '+']
+        parse_exp = l.ParseExpression()
         for op in test_operators:
-            test_str1 = l.add_brackets(op, test_str1)
+            test_str1 = parse_exp.add_brackets(op, test_str1)
         for op in test_operators:
-            test_str2 = l.add_brackets(op, test_str2)
+            test_str2 = parse_exp.add_brackets(op, test_str2)
         for op in test_operators:
-            test_str3 = l.add_brackets(op, test_str3)
+            test_str3 = parse_exp.add_brackets(op, test_str3)
         self.assertEqual(test_str1, result1)
         self.assertEqual(test_str2, result2)
         self.assertEqual(test_str3, result3)
 
     def test_parse_exp(self):
         test_str1 = '3 + 5 - 5 + 3'
+        p_e = l.ParseExpression()
+        result1 = [(100, '('), (100, '('), (200, '3'),
+                   (300, '+'), (100, '('), (200, '5'),
+                   (300, '-'), (200, '5'), (101, ')'),
+                   (101, ')'), (300, '+'), (200, '3'),
+                   (101, ')')]
+        self.assertListEqual(result1, list((i.token_type, i.token_value) for i in p_e.parse_exp(test_str1)))
 
-        result1 = [('скобка', '('), ('скобка', '('), ('число', '3'),
-                   ('операция', '+'), ('скобка', '('), ('число', '5'),
-                   ('операция', '-'), ('число', '5'), ('скобка', ')'),
-                   ('скобка', ')'), ('операция', '+'), ('число', '3'),
-                   ('скобка', ')')]
+    def test_create(self):
+        test_str1 = '3 + 5 - 5 + 3'
+        ast1 = l.AST().create(test_str1)
+        result1 = ['+', '+', '3', '-', '5', '5', '3']
 
-        self.assertListEqual(result1, list((i.token_type, i.token_value) for i in l.parse_exp(test_str1)))
+        test_str2 = '7 + 3 / 25 * ( 5 - 2 )'
+        ast2 = l.AST().create(test_str2)
+        result2 = ['+', '7', '*', '/', '3', '25', '-', '5', '2']
+
+        self.assertListEqual(result1, [i.value for i in ast1.traverse_tree(ast1.root)])
+        self.assertListEqual(result2, [i.value for i in ast2.traverse_tree(ast2.root)])
 
 
 if __name__ == '__main__':
