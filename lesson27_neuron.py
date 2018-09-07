af264a65e14ff6464d198d1aeb8833b23d23935b
Safintim <Sarami96>
2018-09-07 11:43:18 +0300

    create few func and tests

--- lesson27_neuron.py
+++ lesson27_neuron.py
@@ -1,16 +1,33 @@
+import os
+
+
 class Neuron:
     def __init__(self, n, m):
         self.threshold = 35
         self.synapses = [[0] * n for i in range(m)]
 
-    def activation(self, file):
+    @staticmethod
+    def read_file(file):
         list_line = []
-        total = 0
 
         # читаем данные из файла
         with open(file, 'r') as f:
             for line in f:
                 list_line.append([int(char) for char in line.strip()])
+        return list_line
+
+    @staticmethod
+    def correct_wt(arr_wt, arr_in, flag):
+        for i in range(0, len(arr_wt)):
+            for j in range(0, len(arr_wt[i])):
+                if flag:
+                    arr_wt[i][j] += arr_in[i][j]
+                else:
+                    arr_wt[i][j] -= arr_in[i][j]
+        return arr_wt
+
+    def activation(self, list_line):
+        total = 0
 
         # умножаем на соотв вес и суммируем
         for i in range(0, len(self.synapses)):
@@ -22,6 +39,33 @@ class Neuron:
             return 0
         else:
             return 1
+
+    def learning(self, dir_name):
+        list_dir = os.listdir(dir_name)
+        done = 0
+        count = 0
+
+        # пока не распознал все файлы
+        while done != len(list_dir) or count == 10**6:
+            print(count)
+            for i in list_dir:
+                # читаем файл и преобразуем в удобный вид для вычислений
+                list_line = self.read_file(dir_name + '/' + i)
+                # распознавание
+                temp = self.activation(list_line)
+
+                # если это буква отличная от А, но распоз как А
+                if temp == int(i[2]) and 'A' != i[0]:
+                    self.correct_wt(self.synapses, list_line, True)
+                # если это А, но неправильно распоз
+                elif temp != int(i[2]) and 'A' == i[0]:
+                    self.correct_wt(self.synapses, list_line, False)
+                # если все четко
+                elif temp == int(i[2]) and 'A' == i[0]:
+                    done += 1
+            count += 1
+
+        return done, count
 
 # n = Neuron(10,10)
 # print(n.synapses)
--- utest_lesson27.py
+++ utest_lesson27.py
@@ -4,14 +4,38 @@ import os
 
 
 class MyTestCase(unittest.TestCase):
+    def test_read_file(self):
+        n = l.Neuron(10, 10)
+        list_dir = os.listdir('lesson27')
+        b_0 = n.read_file('lesson27/' + list_dir[0])
+        result_str = ''
+        for i in b_0:
+            result_str += ''.join(str(item) for item in i) + '\n'
+        f = open('lesson27/' + list_dir[0], 'r')
+        self.assertTrue(result_str, f.read())
+        f.close()
+
+    def test_correct_wt(self):
+        n = l.Neuron(10, 10)
+        arr_wt = [[1, 1] * 2 for i in range(3)]
+        arr_in = [[1, 1] * 2 for i in range(3)]
+        self.assertListEqual(n.correct_wt(arr_wt, arr_in, False), [[0, 0] * 2 for i in range(3)])
+        self.assertListEqual(n.correct_wt(arr_wt, arr_in, False), [[-1, -1] * 2 for i in range(3)])
+        self.assertListEqual(n.correct_wt(arr_wt, arr_in, True), [[0, 0] * 2 for i in range(3)])
+
     def test_activation(self):
         n = l.Neuron(10, 10)
         result_list = []
         list_dir = os.listdir('lesson27')
         for i in list_dir:
-            result_list.append(n.activation('lesson27/' + i))
-
+            list_line = n.read_file('lesson27/' + i)
+            result_list.append(n.activation(list_line))
         self.assertListEqual(result_list, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_learning(self):
+        n = l.Neuron(10, 10)
+        print(n.learning('lesson27/'))
+
 
 
 if __name__ == '__main__':
