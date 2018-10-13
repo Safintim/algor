da6adcfb67cf9a06417576b17dafaa0baff624f6
Safintim <Sarami96>
2018-10-13 19:22:07 +0300

    create BWT

--- lesson28_BWT.py
+++ lesson28_BWT.py
@@ -1,39 +1,50 @@
-from random import randint
-from collections import namedtuple
+from random import randint, shuffle
 
 """
 Элемент дерева хранит список цветов = [left_child_color, right_child]
 """
 
 
-class Tree:
-    def __init__(self):
-        self.tree = []
+class Node:
+    def __init__(self, colors, flag=None):
+        self.left_child_color, self.right_child_color = colors
+        self.parent_color = None
+        self.colors = colors
+        if flag:
+            self.left_child = None
+            self.right_child = None
 
-    def add_is_valid(self, new_node):
-        parent = (len(self.tree) - 1) // 2
+
+class Tree:
+    def __init__(self, sz):
+        self.tree = [None] * sz
+
+    def add_is_valid(self, index_new_node, new_node):
+        parent = (index_new_node - 1) // 2
         left_child = 2 * parent + 1
         right_child = 2 * parent + 2
-        index_new_node = len(self.tree)
 
-        left = 0
-        right = 1
+        if self.tree[0] is None:
+            return True, None
 
-        if not self.tree:
-            return True
         elif left_child == index_new_node:
-            if self.tree[parent][left] in new_node:
-                return False
+            if self.tree[parent].left_child_color in new_node:
+                return False, None
+            else:
+                return True, self.tree[parent].left_child_color
         elif right_child == index_new_node:
-            if self.tree[parent][right] in new_node:
-                return False
+            if self.tree[parent].right_child_color in new_node:
+                return False, None
+            else:
+                return True, self.tree[parent].right_child_color
 
-        return True
-
-    def add(self, node):
-        if not self.add_is_valid(node):
+    def add(self, index, node):
+        temp = self.add_is_valid(index, node)
+        add_is_valid = temp[0]
+        if not add_is_valid:
             return False
-        self.tree.append(node)
+        self.tree[index] = Node(node)
+        self.tree[index].parent_color = temp[1]
         return True
 
     def __iter__(self):
@@ -48,39 +59,84 @@ class BWT:
         4: 'purple'
     }
 
-    def __init__(self, depth=4, count_colors=4):
-        self.top_tree = Tree()
-        self.bottom_tree = Tree()
+    def __init__(self, depth=3, count_colors=4):
+        self.top_tree = Tree(2 ** depth - 1)
+        self.bottom_tree = Tree(2 ** depth - 1)
         self.relationship_trees = []
         self.depth = depth
         self.count_colors = count_colors
 
-    def generate_random_colors(self):
+    def generate_random_colors(self, exclude=[]):
         colors = set()
         while len(colors) < 2:
             color = randint(1, self.count_colors)
+            if color in exclude:
+                continue
             colors.add(color)
-
         return list(colors)
 
     def coloring_random_tree(self, tree):
-        for count in range(2 ** self.depth - 1):
+        for index, node in enumerate(tree.tree):
+            # листьям не раскрашиваем, но запоминаем цвет родителя
+            if index * 2 + 1 >= len(tree.tree):
+                tree.tree[index] = Node([None, None], flag=True)
+                parent = tree.add_is_valid(index, [None, None])[1]
+                tree.tree[index].parent_color = parent
+                continue
+
             is_valid = False
             while not is_valid:
                 new_node = self.generate_random_colors()
-                is_valid = tree.add(new_node)
+                is_valid = tree.add(index, new_node)
 
         return tree
 
     def coloring_mirror_tree(self, tree1, tree2):
-        self.coloring_random_tree(tree1)
-        tree2 = tree1.copy()
-
+        tree1 = self.coloring_random_tree(tree1)
+        tree2.tree = tree1.tree.copy()
         return tree1, tree2
 
     def create_tree(self, tree):
         """тестовая функция"""
         return self.coloring_random_tree(tree)
 
-    def bind_trees(self):
-        pass
+    def bind_trees(self, tree1, tree2):
+        last_level = -2 ** (self.depth - 1)
+
+        top_tree = tree1.tree[last_level:]
+        bottom_tree = tree2.tree[last_level:]
+
+        while None in [i.right_child for i in top_tree] or\
+                None in [i.right_child for i in bottom_tree]:
+
+            shuffle(top_tree)
+            shuffle(bottom_tree)
+
+            for index in range(len(top_tree)):
+                exclude = [top_tree[index].parent_color,
+                           bottom_tree[index].parent_color]
+                colors = self.generate_random_colors(exclude=exclude)
+
+                top_tree[index].left_child = bottom_tree[index]
+                top_tree[index].left_child_color = colors[0]
+                top_tree[index].right_child_color = colors[1]
+
+                bottom_tree[index].left_child = top_tree[index]
+                bottom_tree[index].left_child_color = colors[0]
+                bottom_tree[index].right_child_color = colors[1]
+
+            for node_top in top_tree:
+                for node_bottom in bottom_tree:
+                    if node_top.right_child_color == node_bottom.right_child_color:
+                        if node_top.left_child != node_bottom and \
+                                node_bottom.left_child != node_top:
+                            node_top.right_child = node_bottom
+                            node_bottom.right_child = node_top
+                            break
+
+        self.relationship_trees = tree1.tree[last_level:] + tree2.tree[last_level:]
+        # return tree1.tree[-2 ** (self.depth - 1):],
+        #  tree2.tree[-2 ** (self.depth - 1):]
+        return self.relationship_trees
+
+
--- utest_lesson28_BWT.py
+++ utest_lesson28_BWT.py
@@ -1,4 +1,5 @@
 import unittest
+import ete3
 import lesson28_BWT as l
 
 
@@ -6,18 +7,111 @@ class MyTestCase(unittest.TestCase):
     def test_generate_random_colors(self):
         bwt = l.BWT()
         for i in range(101):
-            colors = set(bwt.generate_random_colors())
+            colors = set(bwt.generate_random_colors(exclude=[1]))
+            print(colors)
             if len(colors) < 2:
                 print(colors)
                 break
         self.assertEqual(i, 100)
 
+    def test_create_random_tree(self):
+        bwt = l.BWT()
+        bwt.top_tree = bwt.coloring_random_tree(bwt.top_tree)
+        bwt.bottom_tree = bwt.coloring_random_tree(bwt.bottom_tree)
+
+        self.assertNotEqual(bwt.top_tree.tree, bwt.bottom_tree.tree)
+
+    def test_create_mirror_tree(self):
+        bwt = l.BWT()
+        temp = bwt.coloring_mirror_tree(bwt.top_tree, bwt.bottom_tree)
+        self.assertListEqual(temp[0].tree, temp[1].tree)
+
     def test_create_trees(self):
         bwt = l.BWT()
         top = bwt.create_tree(bwt.top_tree)
-        print(top.tree)
-        # print(bottom.tree)
+        for i in top.tree:
+            print('parent {}, left {}, right {}'.format(i.parent_color, i.left_child_color, i.right_child_color))
         self.assertEqual(len(top.tree), 7)
+
+    def test_bind_trees(self):
+        bwt = l.BWT()
+        bwt.top_tree = bwt.coloring_random_tree(bwt.top_tree)
+        bwt.bottom_tree = bwt.coloring_random_tree(bwt.bottom_tree)
+        print('base TOP')
+        for i in bwt.top_tree.tree:
+            print('parent {}, left {}, right {}'.format(i.parent_color,
+                                                        i.left_child_color,
+                                                        i.right_child_color,))
+        print()
+        print('base BOTTOM')
+        for i in bwt.bottom_tree.tree:
+            print('parent {}, left {}, right {}'.format(i.parent_color,
+                                                        i.left_child_color,
+                                                        i.right_child_color,))
+        print()
+        # part_top, part_bottom = bwt.bind_trees(bwt.top_tree, bwt.bottom_tree)
+        bwt.bind_trees(bwt.top_tree, bwt.bottom_tree)
+        part_top = bwt.relationship_trees[-2 ** (bwt.depth - 1):]
+        part_bottom = bwt.relationship_trees[:-2 ** (bwt.depth - 1):]
+        print('TOP')
+        for i in part_top:
+            print('parent {}, left {}, right {}, l_child {}, r_child {}'.format(i.parent_color,
+                                                        i.left_child_color,
+                                                        i.right_child_color,
+                                                        i.left_child,
+                                                        i.right_child))
+        print()
+        print('BOTTOM')
+        for i in part_bottom:
+            print('parent {}, left {}, right {}, l_child {}, r_child {}'.format(i.parent_color,
+                                                        i.left_child_color,
+                                                        i.right_child_color,
+                                                        i.left_child,
+                                                        i.right_child))
+
+        print()
+
+    @staticmethod
+    def format_tree(tree):
+        s = ''
+
+        for i in range(len(tree)):
+            l_c = i * 2 + 1
+            r_c = i * 2 + 2
+            if len(tree) > i * 2 + 2:
+                # print('({},{}){}'.format(
+                #         str(tree[l_c].left_child_color) + 'l-r' + str(tree[l_c].right_child_color),
+                #         str(tree[r_c].left_child_color) + 'l-r' + str(tree[r_c].right_child_color),
+                #         str(tree[i].left_child_color) + 'l-r' + str(tree[i].right_child_color)))
+                if str(tree[i]) in s:
+                    s = s.replace(str(tree[i]), '({},{}){}'.format(
+                        str(tree[r_c].left_child_color) + 'l--r' + str(tree[r_c].right_child_color),
+                        str(tree[l_c].left_child_color) + 'l--r' + str(tree[l_c].right_child_color),
+
+                        str(tree[i].left_child_color) + 'l--r' + str(tree[i].right_child_color)))
+                    continue
+                s = '({},{}){}'.format(
+                    tree[i * 2 + 2],
+                    tree[i * 2 + 1],
+                    str(tree[i].left_child_color) + 'l--r' + str(tree[i].right_child_color))
+        return s
+
+    def test_print(self):
+        # a = l.BWT.COLORS
+        bwt = l.BWT()
+        bwt.top_tree = bwt.coloring_random_tree(bwt.top_tree)
+        bwt.bottom_tree = bwt.coloring_random_tree(bwt.bottom_tree)
+        part_top, part_bottom = bwt.bind_trees(bwt.top_tree, bwt.bottom_tree)
+
+        tree1 = self.format_tree(bwt.top_tree.tree) + ';'
+        tree2 = self.format_tree(bwt.bottom_tree.tree) + ';'
+
+        t1 = ete3.Tree(tree1, format=1)
+        t2 = ete3.Tree(tree2, format=1)
+
+        print(t1.get_ascii(attributes=["name", ]))
+        print()
+        print(t2.get_ascii(attributes=["name", ]))
 
 
 if __name__ == '__main__':
