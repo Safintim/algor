9169be7f3d8c523181cb7ad483d2bed7964d9dbe
Safintim <Sarami96>
2018-11-09 13:59:41 +0300

    create BST functional

--- lesson29_BST.py
+++ lesson29_BST.py
@@ -1,4 +1,4 @@
-from random import randint
+"""1 способ"""
 
 
 class BST:
@@ -25,10 +25,67 @@ class BST:
                 self.tree[i] = halves_arr[i][0]
 
 
-# h = 2
-# b = BST(h=h)
-# test_list = [randint(1, 2**(h+2)) for _ in range(pow(2, h+1) - 1)]
-# print(test_list)
-# b.add(test_list)
-#
-# print('self.tree', b.tree, sep='-')
+"""2 способ, такой же как и первый. только без создания экземпляра"""
+
+
+def halve_arr(arr):
+    halves_arr = [arr]
+
+    for half in halves_arr:
+        if half[:len(half) // 2]:
+            halves_arr.append(half[:len(half) // 2])
+            halves_arr.append(half[len(half) // 2 + 1:])
+    return halves_arr
+
+
+def create_node(key, level):
+    return {'key': key, 'level': level}
+
+
+def arr_to_bst(h, arr):
+    tree = [None] * (pow(2, h + 1) - 1)
+    arr.sort()
+    halves_arr = halve_arr(arr)
+
+    for i in range(len(tree)):
+
+        if len(halves_arr[i]) // 2:
+            tree[i] = create_node(halves_arr[i][len(halves_arr[i]) // 2], None)
+        else:
+            tree[i] = create_node(halves_arr[i][0], None)
+
+        if i == 0:
+            tree[i]['level'] = 1
+        else:
+            tree[i]['level'] = tree[(i - 1) // 2]['level'] + 1
+
+    return tree
+
+
+"""3 способ с классом Node"""
+
+
+class Node:
+    def __init__(self, k):
+        self.left_child = None
+        self.right_child = None
+        self.key = k
+        self.level = None
+
+
+def arr_to_bst2(arr):
+    if not arr:
+        return None
+    mid = len(arr)//2
+    root = Node(arr[mid])
+    root.left_child = arr_to_bst2(arr[:mid])
+    root.right_child = arr_to_bst2(arr[mid+1:])
+    return root
+
+
+def preoder(root):
+    yield root
+    if root.left_child:
+        yield from preoder(root.left_child)
+    if root.right_child:
+        yield from preoder(root.right_child)
--- utest_lessson29_BST.py
+++ utest_lessson29_BST.py
@@ -1,5 +1,8 @@
 import unittest
 import lesson29_BST as l
+import lesson16_bintreeA as l2
+import time
+from random import randint, shuffle
 
 
 class MyTestCase(unittest.TestCase):
@@ -23,6 +26,76 @@ class MyTestCase(unittest.TestCase):
         bst.add(test_list)
 
         self.assertEqual(bst.tree, result)
+
+    @staticmethod
+    def generate_numbers(h):
+        numbers = set()
+        while len(numbers) != pow(2, h+1) - 1:
+            numbers.add(randint(1, 2 ** (h + 2)))
+
+        numbers = list(numbers)
+        shuffle(numbers)
+        return numbers
+
+    def test_speed(self):
+        h = 16
+        test_list = self.generate_numbers(16)
+        bin_a = l2.Tree2(len(test_list))
+        start_bin_a = time.time()
+        for i in test_list:
+            bin_a.add(i)
+        end_bin_a = time.time() - start_bin_a
+
+        bst = l.BST(h=h)
+        start_bst = time.time()
+        bst.add(test_list)
+        end_bst = time.time() - start_bst
+
+        print('BIN_A:', end_bin_a, sep=' ')
+        print('BST:', end_bst, sep=' ')
+
+        self.assertTrue(end_bin_a > end_bst)
+
+    def test_check_keys(self):
+        h = 16
+        test_list = self.generate_numbers(h)
+        tree = l.arr_to_bst(h, test_list)
+        for index, value in enumerate(tree):
+            if index * 2 + 1 >= len(tree):
+                return
+            if tree[index * 2 + 1]['key'] < value['key'] \
+                    <= tree[index * 2 + 2]['key']:
+                pass
+            else:
+                print(tree[index * 2 + 1]['key'], value['key'],
+                      tree[index * 2 + 2]['key'])
+                print('Тревога!!')
+
+    def test_check_balance(self):
+        h = 16
+        test_list = self.generate_numbers(h)
+
+        tree = l.arr_to_bst(h, test_list)
+        last_level = -(2 ** h - 1)
+        last_nodes = tree[last_level:]
+
+        left_subtree = last_nodes[:len(last_nodes) // 2]
+        right_subtree = last_nodes[len(last_nodes) // 2:]
+
+        if not any(left_subtree):
+            for node in left_subtree:
+                node_i = tree.index(node)
+                parent_i = (node_i - 1) // 2
+                if tree[parent_i] is not None:
+                    return 'Баланс'
+        elif not any(right_subtree):
+            for node in right_subtree:
+                node_i = tree.index(node)
+                parent_i = (node_i - 1) // 2
+                if tree[parent_i] is not None:
+                    return 'Баланс'
+        else:
+            return 'ok'
 
 
 if __name__ == '__main__':
