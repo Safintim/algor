f4f6f7cf1fce6caa071c9e7ac319225b8f738205
Safintim <Sarami96>
2018-11-09 13:59:29 +0300

    bug fix

--- lesson16_bintreeA.py
+++ lesson16_bintreeA.py
@@ -18,26 +18,31 @@ class Tree2:
                 index = 2 * index + 1
             else:
                 index = 2 * index + 2
+            if index > len(self.tree):
+                return None
+
             if self.tree[index] is None:
                 return -index
             return self.find(node, index)
 
     def add(self, node):
         index = self.find(node)
-        if index <= 0:
+        if index is None:
+            return 'выход за границы'
+        elif index <= 0:
             self.tree[abs(index)] = node
         else:
-            print('Нода уже есть')
+            return 'Нода уже есть'
 
     def __iter__(self):
         return iter(self.tree)
 
 
-test_list = [8, 4, 12, 2, 6, 1, 3, 5, 7, 10, 14, 9, 11, 13, 15]
-tree = Tree2(len(test_list))
-for i in test_list:
-    tree.add(i)
-
-print(tree.tree)  # [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
-for i in tree:
-    print(i)
+# test_list = [8, 4, 12, 2, 6, 1, 3, 5, 7, 10, 14, 9, 11, 13, 15]
+# tree = Tree2(len(test_list))
+# for i in test_list:
+#     tree.add(i)
+#
+# print(tree.tree)  # [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
+# for i in tree:
+#     print(i)
