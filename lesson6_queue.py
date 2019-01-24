9fa3f247d82513abee3a4a2005862b1f3a903924
Safintim <Sarami96>
2019-01-24 16:45:43 +0300

    FIX

--- lesson6_queue.py
+++ lesson6_queue.py
@@ -1,28 +1,7 @@
-# import lesson5_stack as Stack
+import lesson5_stack as Stack
 """
 Очереди
 """
-
-class Stack:
-
-    def __init__(self):
-        self.stack = []
-
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop(0)
-        return None
-
-    def push(self, value):
-        self.stack.insert(0, value)
-
-    def peek(self):
-        if self.size() > 0:
-            return self.stack[0]
-        return None
-
-    def size(self):
-        return len(self.stack)
 
 
 class Queue:
@@ -59,8 +38,8 @@ def circle_queue(q, n):
 # 6.3
 class Queue2:
     def __init__(self):
-        self.stack1 = Stack()
-        self.stack2 = Stack()
+        self.stack1 = Stack.Stack()
+        self.stack2 = Stack.Stack()
 
     def enqueue(self, item):
         return self.stack1.push(item)
