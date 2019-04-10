cecd27448362da066ad9468515c81c8956af0d7d
Safintim <Sarami96>
2019-04-10 11:40:40 +0300

    fix for sportserver

--- lesson12_cache.py
+++ lesson12_cache.py
@@ -1,29 +1,29 @@
-from lesson10_ass_arr import HashTable
+from lesson10_ass_arr import NativeDictionary
 
 
-class Cache(HashTable):
-    def __init__(self, sz, stp):
-        HashTable.__init__(self, sz, stp)
-        self.count_call = [0] * self.size
+class NativeCache(NativeDictionary):
+    def __init__(self, sz):
+        NativeDictionary.__init__(self, sz)
+        self.hits = [0] * self.size
 
     def put(self, key, value):
         index = self.find(key)
         if index is not None:
             if self.slots[index] == key:
                 self.values[index] = value
-                self.count_call[index] += 1
+                self.hits[index] += 1
                 return True
 
         index = self.seek_slot(key)
         if index is None:
-            old = self.count_call.index(min(self.count_call))
+            old = self.hits.index(min(self.hits))
             self.slots[old] = key
             self.values[old] = value
-            self.count_call[old] = 1
+            self.hits[old] = 1
         else:
             self.slots[index] = key
             self.values[index] = value
-            self.count_call[index] += 1
+            self.hits[index] += 1
         return True
 
     def get(self, key):
@@ -31,5 +31,5 @@ class Cache(HashTable):
         if key is None:
             return None
         else:
-            self.count_call[key] += 1
+            self.hits[key] += 1
             return self.values[key]
--- utest_lesson12_cache.py
+++ utest_lesson12_cache.py
@@ -7,7 +7,7 @@ class CacheTest(unittest.TestCase):
     @staticmethod
     def create_dict():
         test_list = [3, 93, 60, 25, 73, 83, 45, 29, 18, 8, 28, 48, 40, 88, 0, 32, 15]
-        c = l.Cache(17, 3)
+        c = l.NativeCache(17)
 
         for i in range(97, 114):
             c[chr(i)] = test_list[i - 97]
@@ -16,6 +16,9 @@ class CacheTest(unittest.TestCase):
 
     def test_put_get(self):
         c = self.create_dict()
+        print(c.slots)
+        print(c.values)
+        print(c.hits)
         k = 0
         while k < 2:
             for i in range(97, 113):
@@ -35,8 +38,8 @@ class CacheTest(unittest.TestCase):
 
         self.assertTrue(c.is_key('zx'))
         self.assertTrue(c.is_key('zzz'))
-        self.assertTrue(c.count_call[index_zzz], 1)
-        self.assertEqual(c.count_call[index_zx], 4)
+        self.assertTrue(c.hits[index_zzz], 1)
+        self.assertEqual(c.hits[index_zx], 4)
 
 
 if __name__ == '__main__':
