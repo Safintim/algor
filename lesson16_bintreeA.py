1291ba6c16e8d8e207ebf5558236757a5c9d3dd6
Safintim <Sarami96>
2019-04-15 11:04:32 +0300

    fix

--- lesson16_bintreeA.py
+++ lesson16_bintreeA.py
@@ -1,59 +1,8 @@
-"""Бинарное дерево в виде массива"""
-
-
-class Tree2:
-
-    def __init__(self, sz):
-        self.tree = [None] * sz
-
-    def find(self, node, i=0):
-
-        if self.tree[0] is None:
-            return 0
-
-        for index in range(i, len(self.tree)):
-            if self.tree[index] == node:
-                return index
-            elif self.tree[index] > node:
-                index = 2 * index + 1
-            else:
-                index = 2 * index + 2
-            if index > len(self.tree):
-                return None
-
-            if self.tree[index] is None:
-                return -index
-            return self.find(node, index)
-
-    def add(self, node):
-        index = self.find(node)
-        if index is None:
-            return 'выход за границы'
-        elif index <= 0:
-            self.tree[abs(index)] = node
-        else:
-            return 'Нода уже есть'
-
-    def __iter__(self):
-        return iter(self.tree)
-
-
-# test_list = [8, 4, 12, 2, 6, 1, 3, 5, 7, 10, 14, 9, 11, 13, 15]
-# tree = Tree2(len(test_list))
-# for i in test_list:
-#     tree.add(i)
-#
-# print(tree.tree)  # [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
-# for i in tree:
-#     print(i)
-
-
 class aBST:
 
     def __init__(self, depth):
-        # правильно рассчитайте размер массива для дерева глубины depth:
         tree_size = sum(2**i for i in range(depth+1))
-        self.Tree = [None] * tree_size  # массив ключей
+        self.Tree = [None] * tree_size
 
     def FindKeyIndex(self, key):
 
@@ -61,7 +10,6 @@ class aBST:
             return 0
 
         i = 0
-
         while i < len(self.Tree):
             if self.Tree[i] == key:
                 return i
@@ -77,7 +25,7 @@ class aBST:
 
             i = index
 
-        return None  # не найден
+        return None
 
     def AddKey(self, key):
         index = self.FindKeyIndex(key)
