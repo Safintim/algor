3a78261aa4186e0ca7b211f991bc720a13c09d46
Safintim <Sarami96>
2019-04-15 11:02:46 +0300

    fix

--- lesson16_bintreeA.py
+++ lesson16_bintreeA.py
@@ -46,3 +46,45 @@ class Tree2:
 # print(tree.tree)  # [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
 # for i in tree:
 #     print(i)
+
+
+class aBST:
+
+    def __init__(self, depth):
+        # правильно рассчитайте размер массива для дерева глубины depth:
+        tree_size = sum(2**i for i in range(depth+1))
+        self.Tree = [None] * tree_size  # массив ключей
+
+    def FindKeyIndex(self, key):
+
+        if not self.Tree[0]:
+            return 0
+
+        i = 0
+
+        while i < len(self.Tree):
+            if self.Tree[i] == key:
+                return i
+            elif self.Tree[i] > key:
+                index = 2 * i + 1
+            else:
+                index = 2 * i + 2
+
+            if index > len(self.Tree):
+                break
+            if self.Tree[index] is None:
+                return -index
+
+            i = index
+
+        return None  # не найден
+
+    def AddKey(self, key):
+        index = self.FindKeyIndex(key)
+
+        if index is None:
+            return -1
+        elif index <= 0:
+            self.Tree[abs(index)] = key
+
+        return index
