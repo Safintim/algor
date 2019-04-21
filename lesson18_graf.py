718b73f1c47c944cea73eb214d2365ee02f1377f
Safintim <Sarami96>
2019-04-21 22:28:33 +0300

    new graf

--- lesson18_graf.py
+++ lesson18_graf.py
@@ -1,152 +1,211 @@
-from lesson5_stack import Stack
-from lesson6_queue import Queue
+# from lesson5_stack import Stack
+# from lesson6_queue import Queue
+#
+# class SimpleGraph:
+#
+#     def __init__(self, mv):
+#         self.max_vertex = mv
+#         self.m_adjacency = [[0] * mv for _ in range(mv)]
+#         self.vertex = []
+#
+#     def add_vertex(self, v):
+#         self.vertex.append(v)
+#         if len(self.vertex) > self.max_vertex:
+#             [i.append(0) for i in self.m_adjacency]
+#             self.max_vertex += 1
+#             self.m_adjacency.append([0] * self.max_vertex)
+#
+#     def add_edge(self, v1, v2):
+#         if v1 in self.vertex and v2 in self.vertex:
+#             v1_i = self.vertex.index(v1)
+#             v2_i = self.vertex.index(v2)
+#             self.m_adjacency[v1_i][v2_i] = 1
+#             self.m_adjacency[v2_i][v1_i] = 1
+#         else:
+#             print('Какой-то вершины нет')
+#
+#     def remove_edge(self, v1, v2):
+#         if v1 in self.vertex and v2 in self.vertex:
+#             v1_i = self.vertex.index(v1)
+#             v2_i = self.vertex.index(v2)
+#             self.m_adjacency[v1_i][v2_i] = 0
+#             self.m_adjacency[v2_i][v1_i] = 0
+#         else:
+#             print('Какой-то вершины нет')
+#
+#     def remove_vertex(self, v):
+#         if v in self.vertex:
+#             v_i = self.vertex.index(v)
+#             # удаляем столбец вершины
+#             for col in self.m_adjacency:
+#                 col.pop(v_i)
+#             # удаляем строку вершины
+#             self.m_adjacency.pop(v_i)
+#             self.vertex.pop(v_i)
+#             self.max_vertex -= 1
+#         else:
+#             print('Такой вершины нет')
+#
+#     def dfs(self, current, end_path):
+#         # 0 - исходное состояние
+#         stack = Stack()
+#         for v in self.vertex:
+#             v.hit = False
+#
+#         ep_i = self.vertex.index(end_path)  # индекс конечного пути
+#         flag = False
+#
+#         while True:
+#
+#             # если непосещенных больше нет, то пропустить след операции
+#             if not flag:
+#                 # 2-3 - фиксируем как посещенную и добавл в стек
+#                 current.hit = True
+#                 stack.push(current)
+#
+#             c_i = self.vertex.index(current)  # индекс текущей вершины
+#
+#             # 4 - среди смежных ищем целевую вершину
+#             if self.m_adjacency[c_i][ep_i] == 1:
+#                 stack.push(end_path)
+#                 return '->'.join(str(i.name) for i in stack.stack[::-1])
+#
+#             # если нет, то выбираем такую смежную, которая еще не была посещена
+#             for i, value in enumerate(self.m_adjacency[c_i]):
+#                 if value == 1 and not self.vertex[i].hit:
+#                     current = self.vertex[i]
+#                     flag = False
+#                     break  # нужно перейти к 2-3
+#                 elif value == 1:
+#                     flag = True
+#
+#             # 5 - если непосещенных больше нет
+#             if flag:
+#                 stack.pop()  # удаляем верхний элемент стека
+#                 if stack.size() == 0:
+#                     print('Путь не найден')
+#                     return None
+#                 else:
+#                     current = stack.stack[0]  # текущий становится верхний элемент стека
+#
+#     def print_path(self, end):
+#         # если использовать массив предков
+#         # temp = ''
+#         # for i in range(len(p)):
+#         #     if end == start:
+#         #         temp += self.vertex[end].name
+#         #         return temp[::-1]
+#         #     temp += self.vertex[end].name + '-'
+#         #     end = p[end]
+#         if end.parent is None:
+#             return end.name
+#         else:
+#             return self.print_path(end.parent) + end.name
+#
+#     def bfs(self, current, end_path):
+#         # 0 - исходное состояние
+#         queue = Queue()
+#         for v in self.vertex:
+#             v.hit = False
+#             v.parent = None
+#
+#         # parents = [0] * self.max_vertex  # массив предков. p[i] = c_i -> c_i предок i
+#         start_i = self.vertex.index(current)  # индекс начало пути
+#         ep_i = self.vertex.index(end_path)  # индекс конца пути
+#
+#         # parents[start_i] = start_i
+#         flag = False
+#
+#         # 1 - фиксируем текущую как посещенную
+#         current.hit = True
+#
+#         while True:
+#             c_i = self.vertex.index(current)  # индекс текущей вершины
+#
+#             # 2 - из всех смежных вершин выбираем любую непосещенную
+#             for i, value in enumerate(self.m_adjacency[c_i]):
+#                 if value == 1 and not self.vertex[i].hit:
+#                     self.vertex[i].parent = self.vertex[c_i]
+#                     # parents[i] = c_i  # добавляем предка
+#                     flag = False
+#                     self.vertex[i].hit = True
+#                     queue.enqueue(self.vertex[i])
+#                     break
+#                 elif value == 1:
+#                     flag = True
+#
+#             # 2 - если нет непосещенных вершин
+#             if flag:
+#                 # если массив пуст, то значит изучили весь граф и пора печатать путь
+#                 if queue.size() == 0:
+#                     # return self.print_path(parents, start_i, ep_i)
+#                     return self.print_path(end_path)
+#                 else:
+#                     current = queue.dequeue()
+#
+#
+# class Vertex:
+#
+#     def __init__(self, name=None):
+#         self.name = name
+#         self.hit = False
+#         self.parent = None
+#
+
+
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+
 
 class SimpleGraph:
 
-    def __init__(self, mv):
-        self.max_vertex = mv
-        self.m_adjacency = [[0] * mv for _ in range(mv)]
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * self.max_vertex for _ in range(self.max_vertex)]
         self.vertex = []
 
-    def add_vertex(self, v):
+    def AddVertex(self, v):
         self.vertex.append(v)
         if len(self.vertex) > self.max_vertex:
             [i.append(0) for i in self.m_adjacency]
             self.max_vertex += 1
             self.m_adjacency.append([0] * self.max_vertex)
+        return True
 
-    def add_edge(self, v1, v2):
-        if v1 in self.vertex and v2 in self.vertex:
-            v1_i = self.vertex.index(v1)
-            v2_i = self.vertex.index(v2)
-            self.m_adjacency[v1_i][v2_i] = 1
-            self.m_adjacency[v2_i][v1_i] = 1
-        else:
-            print('Какой-то вершины нет')
-
-    def remove_edge(self, v1, v2):
-        if v1 in self.vertex and v2 in self.vertex:
-            v1_i = self.vertex.index(v1)
-            v2_i = self.vertex.index(v2)
-            self.m_adjacency[v1_i][v2_i] = 0
-            self.m_adjacency[v2_i][v1_i] = 0
-        else:
-            print('Какой-то вершины нет')
-
-    def remove_vertex(self, v):
+    def RemoveVertex(self, v):
         if v in self.vertex:
-            v_i = self.vertex.index(v)
-            # удаляем столбец вершины
-            for col in self.m_adjacency:
-                col.pop(v_i)
-            # удаляем строку вершины
-            self.m_adjacency.pop(v_i)
-            self.vertex.pop(v_i)
+            index_remove_vertex = self.vertex.index(v)
+            [i.pop(index_remove_vertex) for i in self.m_adjacency]
+            self.m_adjacency.pop(index_remove_vertex)
+            self.vertex.pop(index_remove_vertex)
             self.max_vertex -= 1
-        else:
-            print('Такой вершины нет')
+            return True
+        return False
 
-    def dfs(self, current, end_path):
-        # 0 - исходное состояние
-        stack = Stack()
-        for v in self.vertex:
-            v.hit = False
+    def IsEdge(self, v1, v2):
+        if v1 in self.vertex and v2 in self.vertex:
+            index_v1 = self.vertex.index(v1)
+            index_v2 = self.vertex.index(v2)
+            if self.m_adjacency[index_v1][index_v2] == 1:
+                return True
+        return False
 
-        ep_i = self.vertex.index(end_path)  # индекс конечного пути
-        flag = False
+    def AddEdge(self, v1, v2):
+        if v1 in self.vertex and v2 in self.vertex:
+            index_v1 = self.vertex.index(v1)
+            index_v2 = self.vertex.index(v2)
+            self.m_adjacency[index_v1][index_v2] = 1
+            self.m_adjacency[index_v2][index_v1] = 1
+            return True
+        return False
 
-        while True:
-
-            # если непосещенных больше нет, то пропустить след операции
-            if not flag:
-                # 2-3 - фиксируем как посещенную и добавл в стек
-                current.hit = True
-                stack.push(current)
-
-            c_i = self.vertex.index(current)  # индекс текущей вершины
-
-            # 4 - среди смежных ищем целевую вершину
-            if self.m_adjacency[c_i][ep_i] == 1:
-                stack.push(end_path)
-                return '->'.join(str(i.name) for i in stack.stack[::-1])
-
-            # если нет, то выбираем такую смежную, которая еще не была посещена
-            for i, value in enumerate(self.m_adjacency[c_i]):
-                if value == 1 and not self.vertex[i].hit:
-                    current = self.vertex[i]
-                    flag = False
-                    break  # нужно перейти к 2-3
-                elif value == 1:
-                    flag = True
-
-            # 5 - если непосещенных больше нет
-            if flag:
-                stack.pop()  # удаляем верхний элемент стека
-                if stack.size() == 0:
-                    print('Путь не найден')
-                    return None
-                else:
-                    current = stack.stack[0]  # текущий становится верхний элемент стека
-
-    def print_path(self, end):
-        # если использовать массив предков
-        # temp = ''
-        # for i in range(len(p)):
-        #     if end == start:
-        #         temp += self.vertex[end].name
-        #         return temp[::-1]
-        #     temp += self.vertex[end].name + '-'
-        #     end = p[end]
-        if end.parent is None:
-            return end.name
-        else:
-            return self.print_path(end.parent) + end.name
-
-    def bfs(self, current, end_path):
-        # 0 - исходное состояние
-        queue = Queue()
-        for v in self.vertex:
-            v.hit = False
-            v.parent = None
-
-        # parents = [0] * self.max_vertex  # массив предков. p[i] = c_i -> c_i предок i
-        start_i = self.vertex.index(current)  # индекс начало пути
-        ep_i = self.vertex.index(end_path)  # индекс конца пути
-
-        # parents[start_i] = start_i
-        flag = False
-
-        # 1 - фиксируем текущую как посещенную
-        current.hit = True
-
-        while True:
-            c_i = self.vertex.index(current)  # индекс текущей вершины
-
-            # 2 - из всех смежных вершин выбираем любую непосещенную
-            for i, value in enumerate(self.m_adjacency[c_i]):
-                if value == 1 and not self.vertex[i].hit:
-                    self.vertex[i].parent = self.vertex[c_i]
-                    # parents[i] = c_i  # добавляем предка
-                    flag = False
-                    self.vertex[i].hit = True
-                    queue.enqueue(self.vertex[i])
-                    break
-                elif value == 1:
-                    flag = True
-
-            # 2 - если нет непосещенных вершин
-            if flag:
-                # если массив пуст, то значит изучили весь граф и пора печатать путь
-                if queue.size() == 0:
-                    # return self.print_path(parents, start_i, ep_i)
-                    return self.print_path(end_path)
-                else:
-                    current = queue.dequeue()
-
-
-class Vertex:
-
-    def __init__(self, name=None):
-        self.name = name
-        self.hit = False
-        self.parent = None
+    def RemoveEdge(self, v1, v2):
+        if v1 in self.vertex and v2 in self.vertex:
+            index_v1 = self.vertex.index(v1)
+            index_v2 = self.vertex.index(v2)
+            self.m_adjacency[index_v1][index_v2] = 0
+            self.m_adjacency[index_v2][index_v1] = 0
+            return True
+        return False
--- utest_lesson18_graf.py
+++ utest_lesson18_graf.py
@@ -6,52 +6,86 @@ class GraphTest(unittest.TestCase):
 
     def test_add_vertex(self):
         g = l.SimpleGraph(4)
-        for i in (l.Vertex() for i in range(5)):
-            g.add_vertex(i)
+        vertexes = [l.Vertex(i) for i in range(4)]
+        for v in vertexes:
+            g.AddVertex(v)
+        self.assertEqual(len(g.m_adjacency), 4)
+        self.assertEqual(len(g.m_adjacency[0]), 4)
+        new_vertex = l.Vertex(4)
+        g.AddVertex(new_vertex)
         self.assertEqual(len(g.m_adjacency), 5)
         self.assertEqual(len(g.m_adjacency[0]), 5)
+        self.assertEqual(g.m_adjacency[0], [0, 0, 0, 0, 0])
 
     def test_add_edge(self):
         g = l.SimpleGraph(5)
-        test_list = [l.Vertex() for i in range(5)]
+        vertexes = [l.Vertex(i) for i in range(5)]
 
-        for i in test_list:
-            g.add_vertex(i)
+        for i in vertexes:
+            g.AddVertex(i)
 
-        g.add_edge(test_list[0], test_list[1])
-        g.add_edge(test_list[4], test_list[1])
+        g.AddEdge(vertexes[0], vertexes[1])
         self.assertListEqual(g.m_adjacency[0], [0, 1, 0, 0, 0])
+        self.assertListEqual(g.m_adjacency[1], [1, 0, 0, 0, 0])
+        g.AddEdge(vertexes[4], vertexes[1])
         self.assertListEqual(g.m_adjacency[1], [1, 0, 0, 0, 1])
         self.assertListEqual(g.m_adjacency[4], [0, 1, 0, 0, 0])
 
-    def test_remove_edge(self):
-        g = l.SimpleGraph(5)
-        test_list = [l.Vertex() for i in range(5)]
-
-        for i in test_list:
-            g.add_vertex(i)
-
-        g.add_edge(test_list[0], test_list[1])
-        g.add_edge(test_list[4], test_list[1])
-        g.remove_edge(test_list[1], test_list[4])
-        self.assertListEqual(g.m_adjacency[1], [1, 0, 0, 0, 0])
-        self.assertListEqual(g.m_adjacency[4], [0, 0, 0, 0, 0])
-
     def test_remove_vertex(self):
         g = l.SimpleGraph(5)
-        test_list = [l.Vertex() for i in range(5)]
+        vertexes = [l.Vertex(i) for i in range(5)]
 
-        for i in test_list:
-            g.add_vertex(i)
+        for v in vertexes:
+            g.AddVertex(v)
 
-        g.add_edge(test_list[0], test_list[1])
-        g.add_edge(test_list[4], test_list[1])
-        g.remove_vertex(test_list[4])
-        self.assertTrue(test_list[4] not in g.vertex)
-        self.assertEqual(len(g.vertex), 4)
-        self.assertEqual(g.max_vertex, 4)
-        self.assertEqual(len(g.m_adjacency), 4)
-        self.assertEqual(len(g.m_adjacency[0]), 4)
+        g.AddEdge(vertexes[0], vertexes[1])
+        g.AddEdge(vertexes[4], vertexes[1])
+        g.RemoveVertex(vertexes[1])
+
+        self.assertListEqual(g.m_adjacency[0], [0, 0, 0, 0])
+        self.assertListEqual(g.m_adjacency[1], [0, 0, 0, 0])
+        self.assertListEqual(g.m_adjacency[2], [0, 0, 0, 0])
+        self.assertListEqual(g.m_adjacency[3], [0, 0, 0, 0])
+
+    def test_remove_edge(self):
+        g = l.SimpleGraph(5)
+        vertexes = [l.Vertex(i) for i in range(5)]
+
+        for i in vertexes:
+            g.AddVertex(i)
+
+        g.m_adjacency = [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 1, 1],
+            [1, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1],
+            [0, 1, 0, 1, 0],
+        ]
+        g.RemoveEdge(vertexes[0], vertexes[1])
+        self.assertListEqual(g.m_adjacency[0], [0, 0, 1, 1, 0])
+        self.assertListEqual(g.m_adjacency[1], [0, 0, 0, 1, 1])
+
+    def test_is_edge(self):
+        g = l.SimpleGraph(5)
+        vertexes = [l.Vertex(i) for i in range(5)]
+
+        for i in vertexes:
+            g.AddVertex(i)
+
+        g.m_adjacency = [
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 1, 1],
+            [1, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1],
+            [0, 1, 0, 1, 0],
+        ]
+        self.assertTrue(g.IsEdge(vertexes[0], vertexes[1]))
+        self.assertFalse(g.IsEdge(vertexes[0], vertexes[4]))
+        self.assertTrue(g.IsEdge(vertexes[3], vertexes[2]))
+        self.assertFalse(g.IsEdge(vertexes[2], vertexes[1]))
+        new_vert = l.Vertex(10)
+        self.assertFalse(g.IsEdge(vertexes[2], new_vert))
+
 
     def test_dfs(self):
 
